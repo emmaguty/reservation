e67c3ea8d2f0fffb0da74770b4566248a60b9371
emmanuelguty <manuel15901@gmail.com>
2023-02-28 20:12:24 -0600

    React Advanced Search Bar Design with a Calendar

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import Header from './components/Header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Header />
   </React.StrictMode>
 );
