0d0ea4697943a6bccb22802cd5675184b8d00599
emmanuelguty <manuel15901@gmail.com>
2023-03-01 13:48:43 -0600

    Email List Component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Featured from '../../components/Featured/Featured';
 import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties';
 import Header from '../../components/Header/Header';
+import MailList from '../../components/MailList/MailList';
 import Navbar from '../../components/Navbar/Navbar';
 import PropertyList from '../../components/PropertyList/PropertyList';
 
@@ -19,6 +20,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Home guest love</h1>
         <FeaturedProperties />
+        <MailList/>
       </div>
     </div>
   )
